7c8f80ec48a0392ff9a407694c95e592bed9fecd
Martin Pritchard <martinpritchard@elevatedirect.com>
2017-04-25 09:09:18 +0100

    Simplify example by removing unrequited pages
    ...Should've really created these ionic apps using the the 'basic'
    template, but forgot.

--- packages/ionic-app-two/src/app/app.module.ts
+++ packages/ionic-app-two/src/app/app.module.ts
@@ -3,10 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -17,10 +14,7 @@ import { AppStateModule } from './app.state.module';
 @NgModule({
     declarations: [
         MyApp,
-        AboutPage,
-        ContactPage,
-        HomePage,
-        TabsPage
+        HomePage
     ],
     imports: [
         BrowserModule,
@@ -31,10 +25,7 @@ import { AppStateModule } from './app.state.module';
     bootstrap: [IonicApp],
     entryComponents: [
         MyApp,
-        AboutPage,
-        ContactPage,
-        HomePage,
-        TabsPage
+        HomePage
     ],
     providers: [
         StatusBar,

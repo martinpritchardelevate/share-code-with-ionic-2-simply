0df0c8fbfcfab43de31186978216b9a7cdefee23
Martin Pritchard <martinpritchard@elevatedirect.com>
2017-04-25 10:22:07 +0100

    Use shared code in the example ionic apps

--- packages/ionic-app-one/src/pages/home/home.ts
+++ packages/ionic-app-one/src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Store } from '@ngrx/store';
+import { IAppState } from '../../app/app.state.module';
+import { TestExtensionActions } from 'shared-ngrx';
 
 @Component({
     selector: 'page-home',
@@ -7,8 +10,10 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-    constructor(public navCtrl: NavController) {
-
+    constructor(
+        private navCtrl: NavController,
+        private store: Store<IAppState>) {
+        this.store.dispatch(new TestExtensionActions().fireTestAction());
     }
 
 }
